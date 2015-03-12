579d8fb3bc5a64874e0c13ea92c67b031465547b
alpha <a0108393@nus.edu.sg>
2015-03-12 18:36:46 +0800

    add parser

--- Tusk/Parser/Parser.cpp
+++ Tusk/Parser/Parser.cpp
@@ -1,7 +1,156 @@
 #include "Parser.h"
 
+
 Parser::Parser(void) {
 }
 
 Parser::~Parser(void) {
+}
+
+
+
+Command Parser::createNewCommand(CommandType extractedCommand){
+	Command newCommand;
+	newCommand.inputCommandType(extractedCommand);
+	return newCommand;
+}
+
+Task Parser::createNewTask(TaskType task, std::string extractedTaskInfo){
+	Task newTask;
+	newTask.inputTaskType(task);
+	newTask.inputTitle(extractedTaskInfo);
+	return newTask;
+}
+	
+
+
+
+std::string Parser::processString(std::string s){
+	std::string processedString = "";
+	size_t pos = 0;
+	std::string delimiter = "/";
+	std::string token;
+
+	while ((pos = s.find(delimiter)) != std::string::npos) {
+    token = s.substr(0, pos);
+	
+	processedString += token;
+    s.erase(0, pos + delimiter.length());
+	pos = s.find(" ");
+	s.erase(0, pos + delimiter.length());
+	}
+
+	return processedString;
+}
+
+// TaskTypes: /d /t /r + FloatingTask
+TaskType Parser::findTaskType(std::string input){
+	TaskType userTaskType = FloatingTask;
+
+	if(input.find("/d") != std::string::npos){
+		userTaskType = Deadline;
+	} else if(input.find("/t") != std::string::npos){
+		userTaskType = TimedTask;
+	} else if(input.find("/re") != std::string::npos){
+		userTaskType = RecurringTask;
+	}
+
+return userTaskType;
+}
+
+std::pair <TaskType, std::string> Parser::extractTaskFromInput(std::string userInput){
+	
+	std::pair <TaskType, std::string> taskPair;
+
+	taskPair.first = findTaskType(userInput);
+	taskPair.second = processString(userInput);
+	
+	return taskPair;
+}
+
+
+bool searchForCreateCommand(std::string input){
+	if(input.find(CREATE_COMMAND) != std::string::npos){
+		return true;
+	}
+	return false;
+}
+
+bool searchForUpdateCommand(std::string input){
+	if(input.find(UPDATE_COMMAND) != std::string::npos){
+		return true;
+	}
+	return false;
+}
+
+bool searchForReadCommand(std::string input){
+	if(input.find(READ_COMMAND) != std::string::npos){
+		return true;
+	}
+	return false;
+}
+
+bool searchForDeleteCommand(std::string input){
+	if(input.find(DELETE_COMMAND) != std::string::npos){
+		return true;
+	}
+	return false;
+}
+
+
+
+
+//	commands to search for /r : remove
+//	/u : update
+//	/c : create
+//	/v : read
+CommandType Parser::extractCommandFromInput(std::string input){
+	CommandType commandType;
+	int numCommands = 0;
+
+	if(searchForCreateCommand(input)){
+	commandType = Create;
+	numCommands++;
+	
+	} else if(searchForUpdateCommand(input)){
+	commandType = Update;
+	numCommands++;
+	
+	} else if(searchForReadCommand(input)){
+	commandType = Read;
+	numCommands++;
+	
+	} else if(searchForDeleteCommand(input)){
+	commandType = Delete;
+	numCommands++;
+	
+	}
+	
+	//TODO: numCommands should be == 1
+	return commandType;
+}
+
+std::pair <Command, Task> Parser::parseUserInput(std::string userInput){
+	std::pair<Command, Task> commandAndTask;
+	
+	CommandType extractedCommand = extractCommandFromInput(userInput);
+	
+	std::string extractedTaskInfo="";
+	std::pair<TaskType, std::string> taskPair = extractTaskFromInput(userInput);
+	
+	_task = createNewTask(taskPair.first,taskPair.second);
+	_command = createNewCommand(extractedCommand);
+
+	commandAndTask.first = _command;
+	commandAndTask.second = _task;
+
+	return commandAndTask;
+}
+
+Command Parser::getCommand(){
+	return _command;
+}
+
+Task Parser::getTask(){
+	return _task;
 }
--- Tusk/Parser/Parser.h
+++ Tusk/Parser/Parser.h
@@ -1,7 +1,41 @@
 #pragma once
+#include "../Tusk/Command.h"
+#include "../Tusk/Task.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+const std::string CREATE_COMMAND = "/c ";
+const std::string UPDATE_COMMAND = "/u ";
+const std::string READ_COMMAND = "/v ";
+const std::string DELETE_COMMAND = "/r ";
 
 class Parser {
-public:
+	private:
+	Task _task;
+	Command _command;
+
+	public:
 	Parser(void);
 	~Parser(void);
+	Command getCommand();
+	Task getTask();
+
+	Command createNewCommand(CommandType);
+	Task createNewTask(TaskType, std::string);
+	std::pair <Command, Task> parseUserInput(std::string);
+
+	std::string processString(std::string);
+
+	TaskType findTaskType(std::string);
+
+	std::pair <TaskType, std::string> extractTaskFromInput(std::string);
+
+	bool searchForCreateCommand(std::string);
+	bool searchForUpdateCommand(std::string);
+	bool searchForReadCommand(std::string);
+	bool searchForDeleteCommand(std::string);
+
+	CommandType extractCommandFromInput(std::string);
 };
