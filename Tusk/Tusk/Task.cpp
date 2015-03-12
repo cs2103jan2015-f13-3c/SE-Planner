f8f2784a3224952e06c9bea4b7c012fae81c02d4
A0108417 <a0108417nus.edu.sg>
2015-03-12 08:40:37 +0800

    inputTask and getTask method

--- Tusk/Tusk/Task.cpp
+++ Tusk/Tusk/Task.cpp
@@ -5,3 +5,11 @@ Task::Task(void) {
 
 Task::~Task(void) {
 }
+
+void Task::inputTask(std::string task) {
+	_task = task;
+}
+
+std::string Task::getTask() {
+	return _task;
+}
--- Tusk/Tusk/Task.h
+++ Tusk/Tusk/Task.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <string>
+
 class Task {
+private:
+	std::string _task;
+
 public:
 	Task(void);
 	~Task(void);
+
+	void inputTask(std::string);
+
+	std::string getTask();
 };
