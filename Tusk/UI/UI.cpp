8f082121d3993dda91774c77a10fa60ceea617a1
A0108417 <a0108417nus.edu.sg>
2015-03-12 14:12:32 +0800

    new method

--- Tusk/UI/UI.cpp
+++ Tusk/UI/UI.cpp
@@ -5,3 +5,11 @@ UI::UI(void) {
 
 UI::~UI(void) {
 }
+
+void UI::inputCommand(std::string command) {
+	_input.inputCommand(command);
+}
+
+std::string UI::getCommand() {
+	return _input.getCommand();
+}
--- Tusk/UI/UI.h
+++ Tusk/UI/UI.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "Input.h"
+#include "Output.h"
+
 class UI {
+private:
+	Input _input;
+	Output _output;
+
 public:
 	UI(void);
 	~UI(void);
+
+	void inputCommand(std::string);
+
+	std::string getCommand();
 };
