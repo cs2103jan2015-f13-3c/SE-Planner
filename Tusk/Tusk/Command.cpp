1cf3eab248a41fcb9afeba4fc1671fabf571cb1e
A0108417 <a0108417nus.edu.sg>
2015-03-12 08:45:11 +0800

    inputCommand and getCommand method

--- Tusk/Tusk/Command.cpp
+++ Tusk/Tusk/Command.cpp
@@ -5,3 +5,11 @@ Command::Command(void) {
 
 Command::~Command(void) {
 }
+
+void Command::inputCommand(std::string command) {
+	_command = command;
+}
+
+std::string Command::getCommand() {
+	return _command;
+}
--- Tusk/Tusk/Command.h
+++ Tusk/Tusk/Command.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <string>
+
 class Command {
+private:
+	std::string _command;
+
 public:
 	Command(void);
 	~Command(void);
+
+	void inputCommand(std::string);
+
+	std::string getCommand();
 };
