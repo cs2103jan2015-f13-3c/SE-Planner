d72e4bf194ae22269a39a9757839c9942b394999
tungnk1993 <tungnk1993@gmail.com>
2015-03-13 09:44:32 +0800

    Read + Display Floating Task

--- Tusk/Logic/Read.cpp
+++ Tusk/Logic/Read.cpp
@@ -1,7 +1,34 @@
 #include "Read.h"
+#include <vector>
+#include <string>
+
+using namespace std;
 
 Read::Read(void) {
 }
 
 Read::~Read(void) {
+}
+
+std::vector<Task> Read::executeCommand(std::vector<Task>& mainTaskList, Task& task)
+{
+	vector<Task> displayedTaskList;
+
+	string keyword = task.getTitle();
+
+	int totalDisplay = 0;
+
+	for (int i = 0; i < mainTaskList.size(); i++)
+	{
+		if (mainTaskList[i].getTitle().find(keyword) != string::npos)
+		{
+			displayedTaskList.push_back(mainTaskList[i]);
+			totalDisplay++;
+		}
+
+		if (totalDisplay == 9) break;
+	}
+
+	return displayedTaskList;
+
 }
