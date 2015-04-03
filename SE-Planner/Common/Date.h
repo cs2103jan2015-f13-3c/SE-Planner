b1e2493cc5b82d452fb471f3256b7c31d302f28e
A0108417 <a0108417nus.edu.sg>
2015-04-03 20:14:28 +0800

    update date class

--- SE-Planner/Common/Date.h
+++ SE-Planner/Common/Date.h
@@ -1,7 +1,30 @@
 #pragma once
 
+enum Month {
+	JANUARY=1, FEBRUARY, MARCH, APRIL, MAY, JUNE,
+	JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+};
+
 class Date {
+private:
+	int _year;
+	Month _month;
+	int _day;
+	bool _isEmpty;
+
 public:
 	Date(void);
 	~Date(void);
+
+	void setYear(int);
+	void setMonth(Month);
+	void setDay(int);
+	void setIsEmpty(bool);
+
+	int getYear();
+	Month getMonth();
+	int getDay();
+	bool getIsEmpty();
+
+	bool isSame(Date);
 };
