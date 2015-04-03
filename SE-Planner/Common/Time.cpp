310610fff9b5f3e517df30b0b1a236fb5352d4e4
A0108417 <a0108417nus.edu.sg>
2015-04-03 20:02:35 +0800

    update time class

--- SE-Planner/Common/Time.cpp
+++ SE-Planner/Common/Time.cpp
@@ -5,3 +5,27 @@ Time::Time(void) {
 
 Time::~Time(void) {
 }
+
+void Time::setHours(int hours) {
+	_hours = hours;
+}
+
+void Time::setMinutes(int minutes) {
+	_minutes = minutes;
+}
+
+int Time::getHours() {
+	return _hours;
+}
+
+int Time::getMinutes() {
+	return _minutes;
+}
+
+bool Time::isSame(Time time) {
+	if ((_hours == time.getHours()) && (_minutes == time.getMinutes())) {
+		return true;
+	} else {
+		return false;
+	}
+}
--- SE-Planner/Common/Time.h
+++ SE-Planner/Common/Time.h
@@ -1,7 +1,19 @@
 #pragma once
 
 class Time {
+private:
+	int _hours;
+	int _minutes;
+
 public:
 	Time(void);
 	~Time(void);
+
+	void setHours(int);
+	void setMinutes(int);
+
+	int getHours();
+	int getMinutes();
+
+	bool isSame(Time);
 };
